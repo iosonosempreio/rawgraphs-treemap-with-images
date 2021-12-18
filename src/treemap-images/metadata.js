ed77771012e83250ff680c1801fb3b2a124e5896
iosonosempreio <iosonosempreio@users.noreply.github.com>
2021-12-18 14:44:15 +0100

    Update metadata.js

--- src/treemap-images/metadata.js
+++ src/treemap-images/metadata.js
@@ -2,13 +2,13 @@ import icon from './treemap.svg'
 import thumbnail from './treemap_thumb.svg'
 
 export const metadata = {
-  name: 'Treemap of images',
+  name: 'Treemap (images)',
   id: 'rawgraphs.treemapImages',
   thumbnail,
   icon,
-  categories: ['hierarchies', 'proportions','images'],
+  categories: ['hierarchies', 'proportions', 'images'],
   description:
-    'It displays hierarchically structured data and a related quantitative dimension. It is composed of an area divided into small rectangles, representing the last level of the tree structure. The rectangles’ size depends on the quantitative dimension.',
-  // code: 'https://observablehq.com/@d3/treemap',
+    "It displays hierarchically structured data and a related quantitative dimension. It is composed of an area divided into small rectangles, representing the last level of the tree structure. The rectangles’ size depends on the quantitative dimension.\nThis model displays images stored in the dataset in base64 format.",
+  code: 'https://github.com/iosonosempreio/rawgraphs-treemap-with-images',
   // tutorial:'https://rawgraphs.io/learning/'
 }
